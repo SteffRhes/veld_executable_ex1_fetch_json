8d05f206c27f7592a69e7fcc586fcc7f7c1a8bdb
steffres <stefan.resch@oeaw.ac.at>
2023-08-07 10:28:11 +0200

    work on better dummy repos

--- src/fetch.sh
+++ src/fetch.sh
@@ -1,8 +1,8 @@
 out_data=/veld/output/a/data.json
-out_log=/veld/output/b/log_fetch.txt
+out_log=/veld/output/b/log.txt
 
 > $out_log
-echo "fetching data at $(date -Is)" >> $out_log
+echo "fetching data at $(date -Is)" 2>&1 | tee $out_log
 rand_int=$(($RANDOM %100 + 1))
-echo "random id: ${rand_int}" >> $out_log
-wget https://dummyjson.com/posts/$rand_int -O $out_data 2>&1 | tee >> $out_log
+echo "random id: ${rand_int}" 2>&1 | tee $out_log
+wget https://dummyjson.com/posts/$rand_int -O $out_data 2>&1 | tee $out_log
