9c52b64da02df91e466fc795fe497efd8c11fd7e
steffres <stefan.resch@oeaw.ac.at>
2023-08-09 11:07:04 +0200

    enable fetch by id, rather than random

--- fetch.sh
+++ fetch.sh
@@ -1,8 +1,1 @@
-out_data=/veld/output/a/data.json
-out_log=/veld/output/b/log.txt
-
-> $out_log
-echo "fetching data at $(date -Is)" 2>&1 | tee $out_log
-rand_int=$(($RANDOM %100 + 1))
-echo "random id: ${rand_int}" 2>&1 | tee $out_log
-wget https://dummyjson.com/posts/$rand_int -O $out_data 2>&1 | tee $out_log
+wget https://dummyjson.com/posts/$id -O /veld/output/data.json
